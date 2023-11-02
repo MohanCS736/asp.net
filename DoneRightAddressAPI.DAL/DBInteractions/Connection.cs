0abffe2017ee182a6e9dbd536992b2de99baf0d3
MohanCS736 <147724427+MohanCS736@users.noreply.github.com>
2023-11-02 18:29:55 +0530

    Revert "add"
    This reverts commit d8c981ef68366c62df700342c5d5cfbc4e5f8602.

--- DoneRightAddressAPI.DAL/DBInteractions/Connection.cs
+++ DoneRightAddressAPI.DAL/DBInteractions/Connection.cs
@@ -7,7 +7,7 @@ namespace DoneRightAddressAPI.DAL.DBInteractions
 {
     public class Connection
     {
-        #region Fieldsssss
+        #region Fields
         public static IConfigurationRoot Configuration { get; set; }
         SqlConnection sqlConnection = null;
         #endregion
